ff2ddd9c5fbed032329927969a2129e8b8cec7ad
Vineeta Singh <vs07312005@gmail.com>
2026-07-17 01:10:43 +0530

    Time: 1260 ms (86.79%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,21 +1,20 @@
 # Write your MySQL query statement below
 (
-  SELECT Users.name AS results
-  FROM MovieRating
-  INNER JOIN Users
+    SELECT Users.name AS results
+    FROM MovieRating
+    INNER JOIN Users
     USING (user_id)
-  GROUP BY user_id
-  ORDER BY COUNT(MovieRating.movie_id) DESC, Users.name
-  LIMIT 1
+    GROUP BY user_id
+    ORDER BY COUNT(MovieRating.movie_id) DESC, Users.name
+    LIMIT 1
 )
 UNION ALL
+
 (
-  SELECT Movies.title AS results
-  FROM MovieRating
-  INNER JOIN Movies
-    USING (movie_id)
-  WHERE DATE_FORMAT(created_at, '%Y-%m') = '2020-02'
-  GROUP BY movie_id
-  ORDER BY AVG(MovieRating.rating) DESC, Movies.title
-  LIMIT 1
-);
+    SELECT m1.title AS results FROM movierating m2
+    INNER JOIN movies m1 ON m2.movie_id=m1.movie_id
+    WHERE DATE_FORMAT(created_at, '%Y-%m') = '2020-02'
+    GROUP BY m2.movie_id
+    ORDER BY AVG(m2.rating) DESC,m1.title ASC LIMIT 1
+)
+
