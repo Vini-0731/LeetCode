fbb560991a2ed8098554e1c3341df211f575ab41
Vineeta Singh <vs07312005@gmail.com>
2026-07-17 20:30:29 +0530

    Time: 750 ms (14.12%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,5 @@
+SELECT product_id, 
+COALESCE((SELECT new_price FROM products p2 
+         WHERE p1.product_id=p2.product_id AND p2.change_date<='2019-08-16' 
+         ORDER BY p2.change_date DESC LIMIT 1),10) AS price 
+FROM (SELECT DISTINCT product_id FROM products)p1
