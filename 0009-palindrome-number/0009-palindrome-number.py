da61ebc0b8f9cdc44e69012fe9cdc7d9977c1daa
Vineeta Singh <vs07312005@gmail.com>
2026-07-23 17:32:12 +0530

    Time: 15 ms (35.14%), Space: 12.6 MB (17.33%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,8 +4,6 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        MAX_INT=2**31-1
-        MIN_INT=-2**31
         is_negative=False
         if x<0:
             is_negative=True
@@ -15,6 +13,6 @@ class Solution(object):
         while num>0:        
             reverse_number=(reverse_number*10)+num%10
             num=num//10
-        if reverse_number<(MIN_INT) or reverse_number>(MAX_INT):
+        if reverse_number<(-(2**31)) or reverse_number>(2**31-1):
             return False    
         return True if dup==reverse_number else False 
