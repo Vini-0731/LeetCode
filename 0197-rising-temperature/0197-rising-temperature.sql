c5ea235492bea831367fafd8aad60881757913ce
Vineeta Singh <vs07312005@gmail.com>
2026-07-27 00:10:48 +0530

    Time: 407 ms (85.91%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT id AS Id FROM (SELECT * ,
-IFNULL(LAG(temperature) over (ORDER BY recorddate ROWS BETWEEN unbounded preceding and unbounded following),temperature) AS pre_temp ,LAG(recordDate) OVER (ORDER BY recordDate) AS pre_date
+IFNULL(LAG(temperature) over (ORDER BY recorddate ),temperature) AS pre_temp ,LAG(recordDate) OVER (ORDER BY recordDate) AS pre_date
 FROM weather) t
 WHERE temperature>t.pre_temp AND DATEDIFF(recordDate, pre_date) = 1;
