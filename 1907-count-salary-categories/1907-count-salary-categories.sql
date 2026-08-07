db1f97d7fc012347e9e8d8c83d39b45c56af449a
Vineeta Singh <vs07312005@gmail.com>
2026-08-07 22:59:42 +0530

    Time: 1823 ms (15.1%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -3,4 +3,4 @@ SELECT 'Low Salary' AS category ,COUNT(account_id) AS accounts_count FROM accoun
 UNION
 SELECT 'High Salary' AS category ,COUNT(account_id) AS accounts_count FROM accounts A1 WHERE income>50000
 UNION
-SELECT 'Average Salary' AS category ,COUNT(account_id) AS accounts_count FROM accounts A1 WHERE income>=20000 AND income<=50000
+SELECT 'Average Salary' AS category ,COUNT(account_id) AS accounts_count FROM accounts A1 WHERE income BETWEEN 20000 AND 50000
