aaaf4181d718eeaab5f9e49a50296cb6a73f7a41
Vineeta Singh <vs07312005@gmail.com>
2026-08-21 00:13:22 +0530

    Time: 405 ms (12.77%), Space: 0B (100%) - LeetHub

--- 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
-SELECT *, IF(dna_sequence LIKE 'ATG%',1,0) AS has_start ,IF(dna_sequence LIKE '%TAA' OR dna_sequence LIKE '%TAG' OR dna_sequence LIKE '%TGA' ,1,0) AS has_stop,IF(dna_sequence LIKE '%ATAT%',1,0) AS has_atat, IF(dna_sequence LIKE '%GGG%',1,0) AS has_ggg
+SELECT *, 
+IF(dna_sequence LIKE 'ATG%',1,0) AS has_start ,
+IF(dna_sequence LIKE '%TAA' OR dna_sequence LIKE '%TAG' OR dna_sequence LIKE '%TGA' ,1,0) AS has_stop,
+IF(dna_sequence LIKE '%ATAT%',1,0) AS has_atat, IF(dna_sequence LIKE '%GGG%',1,0) AS has_ggg
 FROM samples
