436fcac32640f908aa46d9474ee51e43233d455b
Vineeta Singh <vs07312005@gmail.com>
2026-08-25 23:21:12 +0530

    Time: 666 ms (65.39%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
 
-with ok as(select *,row_number() over(partition by customer_id order by order_date) as 'rn'
-,(case when order_date=customer_pref_delivery_date then 1.0 else 0 end ) as 'immediate' from delivery)
+with ok as
+(select *,row_number() over(partition by customer_id order by order_date) as 'rn',
+(case when order_date=customer_pref_delivery_date then 1.0 else 0 end ) as 'immediate' from delivery)
 
-select round(100*sum(immediate)/count(immediate),2) as 'immediate_percentage'
-from ok
-where rn=1
+select round(100*sum(immediate)/count(immediate),2) as 'immediate_percentage' from ok where rn=1
